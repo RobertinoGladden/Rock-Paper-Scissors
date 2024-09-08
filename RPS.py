9fcfcb5d116e371b9f10e8a21178b764aa8d8988
Robertino Gladden Narendra <gladdenrobertino@gmail.com>
2024-09-08 02:32:14 +0000

    initial commit

--- RPS.py
+++ RPS.py
@@ -2,33 +2,27 @@
 
 import random
 
+# Dictionary to define the winning move
 winner = {
-    "R": "P",  
-    "P": "S",  
-    "S": "R"  
+    "R": "P",  # Rock is beaten by Paper
+    "P": "S",  # Paper is beaten by Scissors
+    "S": "R"   # Scissors are beaten by Rock
 }
-
-loser = {
-    "R": "S",  
-    "P": "R",  
-    "S": "P"   
-}
-
 
 def player(prev_play, opponent_history=[]):
+    # Handle the first play case where prev_play is empty
+    if prev_play == "":
+        return random.choice(["R", "P", "S"])
+
     # Maintain a dictionary of opponent's moves
     move_count = {"R": 0, "P": 0, "S": 0}
     
-    if prev_play:
-        # Keep track of opponent's history and count their moves
-        opponent_history.append(prev_play)
-        move_count[prev_play] += 1
+    # Keep track of opponent's history and count their moves
+    opponent_history.append(prev_play)
+    move_count[prev_play] += 1
     
     # Use a simple Markov chain approach to predict opponent's next move
     if len(opponent_history) >= 3:
-        last_two = "".join(opponent_history[-2:])
-        
-        # Most frequent move prediction strategy based on history
         if opponent_history[-1] == opponent_history[-2]:
             prediction = winner[prev_play]
         else:
@@ -37,6 +31,5 @@ def player(prev_play, opponent_history=[]):
     else:
         # Early game random move selection until history builds up
         prediction = random.choice(["R", "P", "S"])
-    
     
     return prediction
